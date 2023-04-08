1643a5a6bb67eff25a7b9cbceec5af459360560b
cghub <cghub0201@gmail.com>
2023-04-08 16:45:59 +0900

    avalidation & distribution update

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,13 +9,14 @@ const prodConfig = {
 
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies,
         }),
