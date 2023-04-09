ce20a576a017896a2fc16972610ff422855d46c2
LEE JUHEON <topofweird@gmail.com>
2023-04-09 20:53:32 +0900

    도메인만 넣기

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: `${domain}/marketing/latest/`,
+        publicPath: `${domain}`,
     },
     plugins: [
         new ModuleFederationPlugin({
