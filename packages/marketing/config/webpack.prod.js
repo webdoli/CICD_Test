4942718eb9dcd6680f703e5ac21ec05aa34283b1
LEE JUHEON <topofweird@gmail.com>
2023-04-09 20:49:16 +0900

    'domain'추가

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -3,11 +3,13 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const packageJson = require('../package.json');
 const commonConfig = require('./webpack.common');
 
+const domain = process.env.PRODUCTION_DOMAIN;
+
 const prodConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/marketing/latest/',
+        publicPath: `${domain}/marketing/latest/`,
     },
     plugins: [
         new ModuleFederationPlugin({
