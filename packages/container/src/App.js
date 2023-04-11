2a20a4c901b41df9b332fcd36bfc7ca9ce56f901
LEE JUHEON <topofweird@gmail.com>
2023-04-11 09:10:39 +0900

    container 컴포넌트 테스트

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TestComp from './components/TestComp';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
     return (
         <div>
+            <TestComp />
             <h1> MarketingApp </h1>
             <hr/>
             <MarketingApp />
