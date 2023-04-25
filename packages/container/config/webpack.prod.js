780e21bb8ceabeabe51bbf86186057d829d2e97d
LEE JUHEON <topofweird@gmail.com>
2023-04-25 14:14:32 +0900

    entry image연결

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -13,7 +13,7 @@ const dashboardDomain = 'https://dashboard-react-prj-f9edc.web.app';
 const prodConfig = {
 
     mode: 'production',
-    entry: ['./src/index.js', './public/images/'],
+    entry: ['./src/index.js', './public/images/cghubLogo.jpg'],
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve('./dist'),
