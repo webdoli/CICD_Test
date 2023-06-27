be652ea3143b151ec0e50f5adff473ac83f2eaa7
cghub <cghub0201@gmail.com>
2023-06-27 19:28:49 +0900

    auth test v2

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -9,7 +9,7 @@ export default ({ history, onSignIn }) => {
 
     return (
         <div>
-            <h3> @ Test 회원가입 페이지 @ </h3>
+            <h3> @ Test 회원가입 페이지 V2 @ </h3>
             <BrowserRouter history={ history }>
                 <Routes >
                     <Route path="/auth/signin/*" element={ <Signin onSignIn={onSignIn}/>} />
